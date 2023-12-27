bcd94bcd1d807e11f05eec87d7d3411454e1ed4e
Hansebin <sebinn052@gmail.com>
2023-12-27 20:31:32 +0900

    design: 기본 전역 스타일 세팅 및 tailwind extends 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      width: {
+        "section-w": "1200px",
+        main: "880px",
+        side: "300px",
+      },
+      height: {
+        header: "100px",
+      },
+      minHeight: {
+        "section-h": "700px",
+        main: "580px",
+        side: "580px",
+      },
+      margin: {
+        "section-my": "calc((100vh - 700px) / 2)",
+      },
+      boxShadow: {
+        basic: "rgba(149, 157, 165, 0.2) 0px 8px 24px;",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
