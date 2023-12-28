75f490d985ab06b5fd70d7f7a88461d098d15ea3
Hansebin <sebinn052@gmail.com>
2023-12-28 22:23:50 +0900

    feat: 현재 날짜 불러오기 및 화면에 출력

--- src/components/ViewDate.tsx
+++ src/components/ViewDate.tsx
@@ -1,3 +1,16 @@
 export default function ViewDate() {
-  return <>ViewDate!</>;
+  // 현재 연월일을 계산하는 함수 fullDate
+  const fullDate = () => {
+    const nowYear = new Date().getFullYear();
+    const nowMonth = `0${new Date().getMonth() + 1}`.slice(-2);
+    const nowDate = `0${new Date().getDate()}`.slice(-2);
+
+    return `${nowYear}.${nowMonth}.${nowDate}`;
+  };
+
+  return (
+    <>
+      <div>{fullDate()}</div>
+    </>
+  );
 }
