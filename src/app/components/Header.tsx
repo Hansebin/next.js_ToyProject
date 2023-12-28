3dda5a3567b023ff901227fb31205b2f85943083
Hansebin <sebinn052@gmail.com>
2023-12-28 22:23:19 +0900

    feat: Header 컴포넌트에서 ViewDate와 ViewWeather 컴포넌트 import
    - 현재 날짜를 알려주는 ViewDate 컴포넌트 import
    - 현재 서울 날씨를 알려주는 ViewWeather 컴포넌트 import
    - 스타일 수정

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,13 @@
+import ViewDate from "@/components/ViewDate";
+import ViewWeather from "@/components/ViewWeather";
+
 export default function Header() {
-  return <div className="w-full h-header shadow-basic rounded-lg">Header</div>;
+  return (
+    <div className="flex items-center justify-between w-full h-header px-60 py-26 text-3xl font-semibold shadow-basic rounded-lg">
+      <div className="flex items-center gap-2">
+        🗓️ <ViewDate />
+      </div>
+      <ViewWeather />
+    </div>
+  );
 }
