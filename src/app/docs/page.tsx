49dedb38761fecc24297ce9d735dc5cb016da88b
Hansebin <sebinn052@gmail.com>
2024-01-01 20:06:13 +0900

    feat: docs 페이지와 intro 페이지 생성 및 초기화
    - docs 페이지 생성 후 초기화
    - intro 페이지 생성 후 초기화
    - docs와 intro에 들어가는 데이터의 경우 따로 분리해서 관리!

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,31 @@
+export default function Docs() {
+  return (
+    <section className="mt-50">
+      <article className="mb-9">
+        <p className="mb-7">Ask 소개</p>
+        <div>
+          Ask는 익명으로 자유롭게 질문을 남길 수 있는 익명 커뮤니티
+          서비스입니다. <br />
+          로그인 없이 언제, 어디서든 궁금한 질문을 자유롭게 남길 수 있습니다.{" "}
+          <br />
+          상대방에게 궁금했던 점을 자유롭게 질문으로 남겨주세요!
+        </div>
+      </article>
+      <article className="mb-9">
+        <p className="mb-7">Ask 이용방법</p>
+        <div>
+          1. Ask Home 페이지로 접속한다. <br />
+          2. 궁금했던 점을 자유롭게 작성하여 질문을 남긴다. <br />
+          3. 본인의 질문이 정상적으로 등록이 되었는지 리스트로 확인한다! <br />
+        </div>
+      </article>
+      <article className="mb-9">
+        <p className="mb-7">Ask 커뮤니티 문화</p>
+        <div>
+          • 고운말, 바른말을 사용해 주세요. <br />
+          • 다른 사람들의 의견을 존중해 주세요. <br />
+        </div>
+      </article>
+    </section>
+  );
+}
---
+++ src/app/intro/page.tsx
@@ -0,0 +1,3 @@
+export default function Intro() {
+  return "페이지 준비중!";
+}
